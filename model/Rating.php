c3ebda1ce54b377438d2f5184fe0f239c24f0762
Thomas Degry <degry.thomas@gmail.com>
2013-06-24 20:57:11 +0200

    Added check for only one vote

--- model/Rating.php
+++ model/Rating.php
@@ -12,6 +12,10 @@ class Rating
     public function rate($post) {
         $sql = 'INSERT INTO mrb_ratings(burger_id, voter_id, rating) VALUES(:burger_id, :voter_id, :rating)';
 
+        if($this->alreadyRated($post['burger_id'], $post['voter_id'])) {
+            return false;
+        }
+
         try {
             $stmt = $this->db->prepare($sql);
             $stmt->bindValue(':burger_id', $post["burger_id"]);
@@ -52,6 +56,25 @@ class Rating
             $stmt->execute();
 
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch(PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public function alreadyRated($burger_id, $voter_id) {
+        $sql = 'SELECT * FROM mrb_ratings WHERE burger_id = :burger_id AND voter_id = :voter_id';
+
+        try {
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindValue(':burger_id', $burger_id);
+            $stmt->bindValue(':voter_id', $voter_id);
+            $stmt->execute();
+
+            if(count($stmt->fetchAll(PDO::FETCH_ASSOC)) > 0) {
+                return true;
+            } else {
+                return false;
+            }
         } catch(PDOException $e) {
             die($e->getMessage());
         }
